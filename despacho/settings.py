8312c93c9dc08e17bea24187f1477a85b3392976
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-11 11:37:13 -0400

    Update settings.py

--- despacho/settings.py
+++ despacho/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['18.207.25.202','127.0.0.1','localhost','3.83.24.216','54.163.1
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://18.207.25.202",
+    "18.207.25.202",
+    "http://127.0.0.1:8000/",
 ]
 
 
