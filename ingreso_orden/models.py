f4a9194512ae86b28677391167b4123c85d7a763
Aranzu <se.riquelme.a@gmail.com>
2021-07-01 10:17:00 -0400

    modifica modelo

--- ingreso_orden/models.py
+++ ingreso_orden/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 class OrdenRetiro(models.Model):
     num_solicitud = models.AutoField(primary_key=True)
-    rut = models.CharField(max_length=100)
+    fecha_ingreso = models.DateTimeField(auto_now_add=True)
     nombre_cl = models.CharField(max_length=100)
+    rut = models.CharField(max_length=100)
+    num_telf = models.CharField(max_length=100)
+    email = models.EmailField()
+    direccion = models.CharField(max_length=100)
     nombre_pro = models.CharField(max_length=500)
     direccion = models.CharField(max_length=100)
+    estado_orden = models.CharField(max_length=100, default='Pendiente')
 
     def __str__(self):
         return self.rut
